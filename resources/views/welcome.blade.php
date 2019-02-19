d8160b2309ccc7d25ca4413a0c876548ba23256f
Shubham Maheshwari <shubhammaheshwari2708@gmail.com>
2019-02-19 14:46:39 +0530

    test 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentationnnnn</a>
+                    <a href="https://laravel.com/docs">Documentationnnnnmbj</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
