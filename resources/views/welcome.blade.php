b12a9b0c638c755b7847412209b78bf9d01514b3
Unknown <prkjha25@gmail.com>
2019-02-19 16:12:22 +0530

    front

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+{{-- <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -95,4 +95,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
